50fd743408714bf34c67300c51417f643ea87c80
bubucuo <276185876@qq.com>
2024-06-04 22:00:26 +0800

    webview可以调试

--- src/screens/WebScreen/index.js
+++ src/screens/WebScreen/index.js
@@ -1,8 +1,11 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/react-in-jsx-scope */
 import WebView from 'react-native-webview';
 
 export default function WebScreen({route}) {
   return (
     <WebView
+      webviewDebuggingEnabled={true}
       style={{margin: 20, padding: 20}}
       source={{uri: route.params.uri || 'https://www.baidu.com/'}}
       originWhitelist={['*']}
