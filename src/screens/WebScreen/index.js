5a64375d18515a40007d4eda5c591aa442950660
bubucuo <276185876@qq.com>
2024-06-04 21:04:49 +0800

    Update index.js

--- src/screens/WebScreen/index.js
+++ src/screens/WebScreen/index.js
@@ -4,7 +4,7 @@ export default function WebScreen({route}) {
   return (
     <WebView
       style={{margin: 20, padding: 20}}
-      source={{uri: route.params.uri || 'https://www.kaikeba.com/'}}
+      source={{uri: route.params.uri || 'https://www.baidu.com/'}}
       originWhitelist={['*']}
       //source={{html: '<h1>出错啦！</h1>'}}
     />
